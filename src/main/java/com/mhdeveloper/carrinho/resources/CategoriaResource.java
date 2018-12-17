a685944a1fd9d3eaca0658e30b7cd90ebda731a8
Marcos Hobson <marcoshobson@gmail.com>
2018-12-17 20:34:26 -0300

    Criada e testada a classe de domínio CATEGORIA

--- src/main/java/com/mhdeveloper/carrinho/resources/CategoriaResource.java
+++ src/main/java/com/mhdeveloper/carrinho/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.mhdeveloper.carrinho.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mhdeveloper.carrinho.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public String listar() {
-		return "Testando REST...";
+	@GetMapping("/listar")
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Eletrodomésticos");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
-
 }
