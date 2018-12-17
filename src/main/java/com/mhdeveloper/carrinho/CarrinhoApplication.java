870256f6049149eeb2e386adc50fb5d4d7f26151
Marcos Hobson <marcoshobson@gmail.com>
2018-12-17 19:33:32 -0300

    Projeto criado

---
+++ src/main/java/com/mhdeveloper/carrinho/CarrinhoApplication.java
@@ -0,0 +1,14 @@
+package com.mhdeveloper.carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoApplication.class, args);
+	}
+
+}
+
